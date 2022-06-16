4fbb7a55bbe3b15d07eb3405182a4704add2b8df
amaboh <amabocanon@gmail.com>
2022-06-16 23:59:53 +0100

    board clone and sign in page for admin set up

--- board/src/pages/login/Login.jsx
+++ board/src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
-import "./login.css";
+import "./login.scss";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
